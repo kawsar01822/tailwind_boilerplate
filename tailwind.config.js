c2fd06fb51a4de97c875eaee073ba86a57a40a3f
Kawsar Ahmed <rayhanul.islam@ibcs-primax.com>
2024-05-27 16:54:29 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // This includes all HTML files in the root directory
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
